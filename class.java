4a1fce8b7ba2632ff36ce0062e9d9a3ffec183a7
ariful2000 <57025439+ariful2000@users.noreply.github.com>
2022-12-11 22:09:20 -0500

    Add files via upload

---
+++ class.java
@@ -0,0 +1,25 @@
+import java.io.File;
+
+class Token {
+    String lexem;
+    int tkn;
+}
+
+class Compiler {
+    Compiler (File input) {
+        Lexer lex = Lexer();
+        Parser parser;
+    }
+}
+
+class Lexer {
+    Lexer (String input) {
+
+    }
+}
+
+class Parser {
+    Parser (String input) {
+
+    }
+}
